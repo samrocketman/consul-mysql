8b91a381cce784d856aafbc46ba2de35dfaa690b
Sam Gleske <sam.mxracer@gmail.com>
2020-05-27 00:22:38 -0400

    Bind MySQL on all ports and create remote admin
    This way MySQL can be accessed from a location other than localhost.  The
    remote admin credentials are uploaded to Vault and the root credentials
    are left to be local only.

--- service/run_mysql.sh
+++ service/run_mysql.sh
@@ -8,8 +8,8 @@ function upload_vault_creds() (
   set_vault_infra_token
   execute_vault_command \
     vault kv put docker/mysql-admin \
-      user=root \
-      password="$(< /mnt/mysql_root_pass)"
+      user="$(< /mnt/mysql_admin_user)" \
+      password="$(< /mnt/mysql_admin_password)"
   revoke_self
   echo 'Wrote vault credentials to docker/mysql-admin'
 )
@@ -18,6 +18,21 @@ set -ex
 
 function rand_password() {
   tr -dc -- '-;.~,.<>[]{}!@#$%^&*()_+=`0-9a-zA-Z' < /dev/urandom | head -c64;echo
+}
+
+function create_sql_admin() {
+  echo 'Creating remote SQL Admin credentials.'
+  ADMIN_USER=sqladmin
+  ADMIN_PASSWORD="$(rand_password)"
+  touch /mnt/mysql_admin_user /mnt/mysql_admin_password
+  chmod 600 /mnt/mysql_admin_user /mnt/mysql_admin_password
+  echo "${ADMIN_USER}" > /mnt/mysql_admin_user
+  echo "${ADMIN_PASSWORD}" > /mnt/mysql_admin_password
+  mysql -u root -p"$INITIAL_ROOT_PASSWORD" <<EOF
+CREATE USER '${ADMIN_USER}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${ADMIN_USER}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';
+EOF
+  upload_vault_creds
 }
 
 # initialize database if not already initialized
@@ -33,8 +48,6 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     echo "$INITIAL_ROOT_PASSWORD" > "$passfile"
     unset passfile
   fi
-  echo 'Wrote passwords to:'
-  ls -1 /mnt/mysql_* | sed 's/^/    /'
   echo 'Starting initial mysql daemon.'
   mysqld_safe --basedir=/usr &
   MYSQL_PID=$!
@@ -43,13 +56,15 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   until mysqladmin ping; do sleep 5; done
   echo 'Changing root password.'
   mysqladmin -u root password "$INITIAL_ROOT_PASSWORD"
+  create_sql_admin
   echo 'Shutting down initial mysql daemon.'
   kill $(pgrep -P $MYSQL_PID)
   while pgrep -P $MYSQL_PID;do
     sleep 1
     echo 'Waiting for shutdown...'
   done
-  upload_vault_creds
+  echo 'Wrote passwords to:'
+  ls -1 /mnt/mysql_* | sed 's/^/    /'
   set -x
 fi
 
@@ -57,7 +72,7 @@ rm -f /var/log/mariadb/mariadb.log
 mkdir -p /var/log/mariadb
 mkfifo /var/log/mariadb/mariadb.log
 chown mysql: /var/log/mariadb /var/log/mariadb/mariadb.log
-/usr/bin/mysqld_safe --basedir=/usr &
+/usr/bin/mysqld_safe --bind-address 0.0.0.0 --basedir=/usr &
 MYSQL_PID=$!
 
 trap "kill -SIGQUIT $MYSQL_PID" INT
